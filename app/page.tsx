18de959ceab9ac9f99a5362925e748c67b2611dd
Anjana Paul <anjanapaul0614@gmail.com>
2025-11-28 14:54:32 +0530

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Hero from "./components/Hero";
-import Container from "./components/Container";
-import About from "./components/About";
-
-
 
 export default function Home() {
 return (
 <>
+<Hero/>
 </>
 );
 }
